dcec87b6812da3b19b095b3b2921948ea46b9812
sandeepmunday <109511608+sandeepmunday@users.noreply.github.com>
2022-07-27 14:04:12 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\HP\OneDrive\Desktop\Assignment-4\9.py ===========
+Vehicle object destroyed
+Exception ignored in: <function Vehicle.__del__ at 0x000001D61201B010>
+Traceback (most recent call last):
+  File "C:\Users\HP\OneDrive\Desktop\Assignment-4\9.py", line 4, in __del__
+    print(10/0)
+ZeroDivisionError: division by zero
